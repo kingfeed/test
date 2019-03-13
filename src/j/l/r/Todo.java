177317c2858598a866ff26f99c80e6c60e3aac42
dongfei <df@choicesoft.com.cn>
2019-03-13 17:56:10 +0800

    删除本地化内容

---
+++ src/j/l/r/Todo.java
@@ -0,0 +1,12 @@
+package j.l.r;
+
+import java.lang.annotation.*;
+
+/**
+ * @author dongfei
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Todo {
+}
