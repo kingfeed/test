4c79ab3dd1f08bb83790d7ca0e0c1a0c218fed5e
dongfei <df@choicesoft.com.cn>
2019-07-23 14:57:13 +0800

    LinkedHashSet,维持add顺序

---
+++ src/main/java/GoodsTraceStockBillGroupEnum.java
@@ -0,0 +1,23 @@
+/**
+ * 业务单据分组
+ * @author dongfei
+ */
+public enum GoodsTraceStockBillGroupEnum {
+    /**
+     * 生产业务单据
+     */
+    PRODUCTION,
+    /**
+     * 销售业务单据
+     */
+    SALE,
+    /**
+     * 领料申请单据
+     */
+    PICKING_APPLY,
+    /**
+     * 其他单据
+     */
+    OTHER
+
+}
