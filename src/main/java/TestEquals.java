93baf1ef8c6a3e20d75177f11e568e49cd2e5641
dongfei <df@choicesoft.com.cn>
2019-09-05 17:59:36 +0800

    Integer 自动解包测试

--- src/main/java/TestEquals.java
+++ src/main/java/TestEquals.java
@@ -1,8 +1,33 @@
+import lombok.Data;
+import lombok.Getter;
+
 public class TestEquals {
     public static void main(String[] args){
         int i =1;
+        Integer k = new Integer(1);
         Integer j = new Integer(1);
-        System.out.println(i == j);
-        System.out.println(j.equals(i));
+        A a = new A();
+        a.setA(1);
+        A b = new A();
+        b.setA(1);
+        System.out.println(a.getA() == B.B.getB());//true
+        System.out.println(a.getA() == b.getA());//true
+        System.out.println(i == j);//true
+        System.out.println(k == j);//false
+        System.out.println(j.equals(i));//true
+    }
+
+    @Data
+    public static class A{
+        private Integer a;
+    }
+
+    @Getter
+    protected enum B{
+        B(1);
+        Integer b;
+       B(int c){
+            this.b = c;
+       }
     }
 }
